106c260d730763005411818a927593c78204942b
Alex P <alex.pantec@gmail.com>
2019-09-26 12:52:49 +0300

    refactor pretty

--- src/formatters/pretty.php
+++ src/formatters/pretty.php
@@ -2,6 +2,7 @@
 
 namespace Differ\formatters\pretty;
 
+use function Funct\Collection\flatten;
 use const Differ\ADDED;
 use const Differ\NESTED;
 use const Differ\REMOVED;
@@ -11,10 +12,7 @@ use const Differ\UNCHANGED;
 const TYPE_OBJECT = 'object';
 const TYPE_PROPERTY = 'property';
 
-function stateToName($state)
-{
-    return strtoupper("_${state}_");
-}
+const INDENT_SIZE = 4;
 
 function getValueType($node)
 {
@@ -39,63 +37,83 @@ function prettifyJson($json)
     return removeCommas(removeQuotes($json));
 }
 
-function formatValue($value)
+function stateToChar($state)
 {
-    $jsonText = json_encode($value, JSON_PRETTY_PRINT);
-    return json_encode($value);
-}
-
-function traverse($node)
-{
-    $valueType = getValueType($node);
-    switch ($valueType) {
-        case TYPE_OBJECT:
-            switch ($node['state']) {
-                case UNCHANGED:
-                    return $node['oldValue'];
-                case NESTED:
-                    $properties = array_merge(...array_map(function ($property) {
-                        return traverse($property);
-                    }, $node['children']));
-                    return (object)$properties;
-            }
-            throw new \Exception("Unsupported OBJECT state ${node['state']}");
-        case TYPE_PROPERTY:
-            $name = $node['name'];
-            switch ($node['state']) {
-                case UNCHANGED:
-                    return [$name => $node['oldValue']];
-                case ADDED:
-                    return [stateToName($node['state']) . $name => $node['newValue']];
-                case REMOVED:
-                    return [stateToName($node['state']) . $name => $node['oldValue']];
-                case CHANGED:
-                    return [
-                        stateToName(REMOVED) . $name => $node['oldValue'],
-                        stateToName(ADDED) . $name => $node['newValue']
-                    ];
-                case NESTED:
-                    return [$name => traverse($node['children'][0])];
-            }
-            throw new \Exception("Unsupported PROPERTY state ${node['state']}");
+    switch ($state) {
+        case ADDED:
+            return '+';
+        case REMOVED:
+            return '-';
+        case UNCHANGED:
+            return ' ';
     }
-    throw new \Exception("Unsupported type ${node['type']}");
+    throw new \Exception("Unsupported state $state");
 }
 
-function format($diffTree)
+function indent($level, $state = UNCHANGED, $size = INDENT_SIZE)
 {
-    $json = traverse($diffTree[0]); // TODO: refactor
+    if ($level === 0) {
+        return '';
+    }
+    if ($level < 0) {
+        throw new \Exception("Indent level cannot be less than 0, $level given");
+    }
+    if ($size < 2) {
+        throw new \Exception("Indent size cannot be less than 2, $size given");
+    }
+    return str_repeat(' ', $level * $size - 2) . stateToChar($state) . ' ';
+}
 
-    $jsonText = prettifyJson(json_encode($json, JSON_PRETTY_PRINT));
-    $diffText = str_replace(
-        [
-            '  ' . stateToName(ADDED),
-            '  ' . stateToName(REMOVED)],
-        [
-            '+ ',
-            '- '
-        ],
-        $jsonText
-    );
-    return $diffText;
+function formatValue($value, $level)
+{
+    $json = json_encode($value, JSON_PRETTY_PRINT);
+    $prettyJson = prettifyJson($json);
+    $lines = explode(PHP_EOL, $prettyJson);
+    $indentedLines = flatten([ // no ... here until 7.4 :(
+        $lines[0],
+        array_map(function ($line) use ($level) {
+            return indent($level) . $line;
+        }, array_slice($lines, 1))
+    ]);
+    return implode(PHP_EOL, $indentedLines);
+}
+
+function format($nodes, $level = 0)
+{
+    if (empty($nodes)) {
+        return null;
+    }
+
+    $lines = array_map(function ($node) use ($level) {
+        $valueType = getValueType($node);
+        switch ($valueType) {
+            case TYPE_OBJECT:
+                switch ($node['state']) {
+                    case NESTED:
+                        return '{' . PHP_EOL .
+                            format($node['children'], $level + 1) . PHP_EOL .
+                            indent($level) . '}';
+                }
+                throw new \Exception("Unsupported OBJECT state ${node['state']}");
+            case TYPE_PROPERTY:
+                $name = $node['name'];
+                switch ($node['state']) {
+                    case UNCHANGED:
+                        return indent($level) . "$name: " . formatValue($node['oldValue'], $level);
+                    case ADDED:
+                        return indent($level, ADDED) . "$name: " . formatValue($node['newValue'], $level);
+                    case REMOVED:
+                        return indent($level, REMOVED) . "$name: " . formatValue($node['oldValue'], $level);
+                    case CHANGED:
+                        return indent($level, REMOVED) . "$name: " . formatValue($node['oldValue'], $level) . PHP_EOL .
+                            indent($level, ADDED) . "$name: " . formatValue($node['newValue'], $level);
+                    case NESTED:
+                        return indent($level) . "$name: " . format($node['children'], $level);
+                }
+                throw new \Exception("Unsupported PROPERTY state ${node['state']}");
+        }
+        throw new \Exception("Unsupported type ${node['type']}");
+    }, $nodes);
+
+    return implode(PHP_EOL, array_filter($lines));
 }
